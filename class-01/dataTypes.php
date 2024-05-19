12a70fab7a76a522c9b44f35389ba3bba38ee1ca
laxmankrishnamurti <freelancing.laxman@gmail.com>
2024-05-19 14:56:23 +0530

    Making changes in the file

--- class-01/dataTypes.php
+++ class-01/dataTypes.php
@@ -1,10 +1,13 @@
 <?php
 $userName = "Laxman Krishnamurti";
-echo var_dump($userName);
+// echo var_dump($userName);
 
 const userAge = 21;
-echo var_dump(userAge);
+// echo var_dump(userAge);
 
 const members = ["Jitendra", "Rani", "Laxman", "Anjali", "Fruti", "Vikram"];
 // echo var_dump(members);
-?>
+
+echo "<h1>Hello</h1>"
+
+    ?>
