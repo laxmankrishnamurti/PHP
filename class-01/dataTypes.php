e2387ae05e15b9f09da711fc6fc2c2ddfa55d5ca
laxmankrishnamurti <freelancing.laxman@gmail.com>
2024-05-19 14:04:15 +0530

    Adding new files

---
+++ class-01/dataTypes.php
@@ -0,0 +1,10 @@
+<?php
+$userName = "Laxman Krishnamurti";
+echo var_dump($userName);
+
+const userAge = 21;
+echo var_dump(userAge);
+
+const members = ["Jitendra", "Rani", "Laxman", "Anjali", "Fruti", "Vikram"];
+// echo var_dump(members);
+?>
--- class-01/helloWorld.php
+++ class-01/helloWorld.php
@@ -1,3 +1,13 @@
 <?php
-echo "Hello world!!!"
-    ?>
+// echo "Hello world!!! <br/>";
+
+// $name = "Laxman Krishnamurti";
+// $Name = "Sonu Kumar";
+// $NAME = "Harshad Mehta";
+
+// echo "name value is : $name <br/>";
+// echo "Name value is : $Name <br/>";
+// echo "NAME value is : $NAME <br/>";
+
+echo phpinfo();
+?>
