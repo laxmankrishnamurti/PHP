194b6c8a460e604fa32076207c9ef52bbcb76510
laxmankrishnamurti <freelancing.laxman@gmail.com>
2024-05-25 22:37:17 +0530

    Adding new files

---
+++ Revision/Request/server/upload_files.php
@@ -0,0 +1,12 @@
+<?php
+
+if (isset($_FILES['file'])) {
+    $upload_path = './assets/' . $_FILES['file']['name'];
+    if (move_uploaded_file($_FILES['file']['tmp_name'], $upload_path)) {
+        echo "File uploaded successfully";
+    } else {
+        echo "Failed to upload";
+    }
+} else {
+    echo "No file is selected";
+}
