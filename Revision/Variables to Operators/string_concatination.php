bd1e13b87afd65bafa1fa2058580d7a8bdb332b1
laxmankrishnamurti <freelancing.laxman@gmail.com>
2024-05-25 16:14:33 +0530

    Adding new files'

---
+++ Revision/Variables to Operators/string_concatination.php
@@ -0,0 +1,15 @@
+<?php
+
+$name = "Laxman Krishnamurti";
+echo "My name is " . $name;
+
+//Output :- My name is Laxman Krishnamurti
+
+
+$age = 21;
+;
+
+echo "<br/>";
+echo "My name is " . $name . " and I'm " . $age . " Years old";
+
+//Output :-  My name is Laxman Krishnamurti and I'm 21 Years old
