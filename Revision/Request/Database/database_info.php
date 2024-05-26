56068403bcb098c5798dbe1814399a8f501de4ea
laxmankrishnamurti <freelancing.laxman@gmail.com>
2024-05-26 06:11:13 +0530

    databases starts

---
+++ Revision/Request/Database/database_info.php
@@ -0,0 +1,22 @@
+/**
+What is Database?
+
+Database is the storage localtion where we store data. There are two types of databases, these are :-
+(1). Relational databases
+(2). Non-releational databases
+
+(*) Releational databases :- The database which store data in the form of table. and all data are releated to each
+other.
+
+Ex:- MySQL, Postgres, MariaDB.....etc
+
+(*) Non-releational databases :- The database which store data in the form of documents. And all data are mixed.
+
+Ex:- MongoDB.......etc
+
+
+Which type of databbase is suitable for PHP?
+
+Well, the answer of this question is MySQL. Because when we install XAMMP, PHPMyAdmin is being installed automatically.
+
+*/
