c63699ab2e5ce9528d33954bd98676837a3f35b7
laxmankrishnamurti <freelancing.laxman@gmail.com>
2025-01-29 17:49:56 +0530

    Insert Operating in MySQL Database

---
+++ Getting Started/02-Guide/15-insert_operation.php
@@ -0,0 +1,18 @@
+<?php
+
+include("./config/database_connection.php");
+
+// INSERT Query
+
+$date = date("Y-m-d H:i:s");
+$sql = "INSERT into users (username, password, created_at) values ('Harshad Mehta', 'harshad@123','$date')";
+
+$result = $conn->query($sql);
+if ($result) {
+    echo "Signed up successfully!";
+} else {
+    echo "Signup failed!, Err :: " . $conn->error;
+}
+
+
+echo $lskadjf;
